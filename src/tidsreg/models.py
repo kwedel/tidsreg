c79cc2335bd792f792d413d0acafc46a842bb939
Kåre Obel Wedel <sg4l@kk.dk>
2024-10-13 12:46:28 +0200

    Finish implementation of get_registrations

--- src/tidsreg/models.py
+++ src/tidsreg/models.py
@@ -1,6 +1,8 @@
 import datetime
 from dataclasses import dataclass
 
+from playwright.sync_api import Page
+
 
 @dataclass
 class Registration:
@@ -19,3 +21,18 @@ class Registration:
 
     def _format_time(self, time):
         return f"{time.hour:02}:{time.minute:02}"
+
+
+class RegistrationDialog:
+    """Pop-up registration dialog"""
+
+    def __init__(self, page: Page):
+        self.dialog = page.frame_locator("#dialog-body")
+        self.start = self.dialog.locator(
+            "#NormalContainer_NormalTimePnl_NormalTimeStart"
+        )
+        self.slut = self.dialog.locator("#NormalContainer_NormalTimePnl_NormalTimeEnd")
+        self.kommentar = self.dialog.get_by_role("textbox", name="Til personligt notat")
+        self.ok_button = self.dialog.get_by_role("button", name="Ok")
+        self.annullere_button = self.dialog.get_by_role("button", name="Annullere")
+        self.slet_button = self.dialog.get_by_role("button", name="Slet")
